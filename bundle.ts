025492d3f77bdc9751e1b9740bf1f1f2575fcccd
grinevich <yuriy.grinevich@novage.com.ua>
2021-01-21 16:27:41 +0200

    Fix bundle.ts, avoid Deno issue #9055

--- bundle.ts
+++ bundle.ts
@@ -6,6 +6,11 @@ const source = files['deno:///bundle.js'].replace(
     new Date().toISOString().split('-').reduce((a, c) => a + (c.length == 4 ? c.substring(2) : c.length > 4 ? c.substring(0, 2) : c), '')
 )
 
+// fix incorrect js generation; last tested in Deno 1.7.0
+// issue details: https://github.com/denoland/deno/issues/9055
+// TODO: remove when Deno gets a fix
+.replace('__int: __int1', 'int: __int1')
+
 console.log('Writing bundle...')
 Deno.writeTextFileSync('./docs/game.js', source)
 
