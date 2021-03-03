b75e62b33f8afd393ad7ba32efe41d344c5e5377
grinevich <yuriy.grinevich@novage.com.ua>
2021-03-03 14:35:58 +0200

    bundle: display issues if any (was silent "done")

--- bundle.ts
+++ bundle.ts
@@ -1,12 +1,20 @@
 console.log('Generating bundle...')
 
-const { files } = await Deno.emit('./src/game.ts', { bundle: 'esm', compilerOptions: { checkJs: true } })
-const source = files['deno:///bundle.js'].replace(
-    /(\[BUILDSTAMP\])/g,
-    new Date().toISOString().split('-').reduce((a, c) => a + (c.length == 4 ? c.substring(2) : c.length > 4 ? c.substring(0, 2) : c), '')
-)
+const { diagnostics, files } = await Deno.emit('./src/game.ts', { bundle: 'esm' })
 
-console.log('Writing bundle...')
-Deno.writeTextFileSync('./docs/game.js', source)
+if (diagnostics.length == 0) {
+    const buildstamp = new Date()
+        .toISOString()
+        .split('-')
+        .reduce((a, c) => a + (c.length == 4 ? c.substring(2) : c.length > 4 ? c.substring(0, 2) : c), '')
 
-console.log('All done.')
+    console.log(`Replacing buildstamp with "${buildstamp}"...`)
+    const source = files['deno:///bundle.js'].replace(/(\[BUILDSTAMP\])/g, buildstamp)
+
+    console.log('Writing bundle...')
+    Deno.writeTextFileSync('./docs/game.js', source)
+
+    console.log('All done.')
+} else {
+    console.log('Issues', diagnostics)
+}
